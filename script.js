ffbd018f06189bdab7b11d55f007ff46ffa34601
Adithya <adithyaaneesh29@gmail>
2025-07-19 12:28:51 +0530

    progress

--- script.js
+++ script.js
@@ -12,9 +12,9 @@
 
     data.map((item)=> {
         specialityList = specialityList + `<div class="card">
-                                    <h3>${item.title}</h3>
-                                    <p>${item.subtitle}</p>
-                                    </div>`
+                                            <h3>${item.title}</h3>
+                                            <p>${item.subtitle}</p>
+                                            </div>`
 
     })
 
@@ -33,19 +33,22 @@
             category:'Groceries',
             name:'Hand Sanitizer',
             rating:'3star',
-            price:'$55'},
+            price:'$55'
+        },
         {
-            image:'https://websitedemos.net/organic-shop-02/wp-conten…ploads/sites/465/2018/06/red-chillies-600x600.jpg',
+            image:'https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/red-chillies-600x600.jpg',
             category:'Groceries',
             name:'Handpicked Red Chillies',
             rating:'4star',
-            price:'$65'},
+            price:'$65'
+        },
         {
             image:'https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/edible-oil-600x600.jpg',
             category:'Groceries',
             name:'Natural Extracted Edible Oil',
             rating:'4star',
-            price:'$25'},
+            price:'$25'
+        },
     ];
 
     const productContainer = document.getElementById('productContainer');
@@ -56,17 +59,93 @@
        productList = productList + `<div class="product">
                                     <img src="${item.image}"></img>
                                     <p>${item.category}</p>
-                                    <p>${item.name}</p>
+                                    <h5>${item.name}</h5>
                                     <p>${item.rating}</p>
                                     <p>${item.price}</p>
                                     </div>`
 
     })
-    console.log(productContainer)
     productContainer.innerHTML = productList
 
+    const listItem = [
+        {
+            contentTitle:'Farm Fresh Fruits',
+            subtitle:'Ut sollicitudin quam vel purus tempus, vel eleifend felis varius.',
+            button:'SHOP NOW',
+            background:'img'
+        },
+        {
+            contentTitle:'Fresh Vegetables',
+            subtitle:'Aliquam porta justo nibh, id laoreet sapien sodales vitae justo.',
+            button:'SHOP NOW',
+        },
+        {
+            contentTitle:'Organic Legume',
+            subtitle:'Phasellus sed urna mattis, viverra libero sed, aliquam est.',
+            button:'SHOP NOW',
+        },
+    ];
+
+    const itemContainer = document.getElementById('itemContainer');
+
+    let itemList = ''
+
+    listItem.map((item)=> {
+        itemList = itemList + `<div class="list">
+                                <h3>${item.contentTitle}</h3>
+                                <p>${item.subtitle}</p>
+                                <button>${item.button}</buttton>
+                                </div>`
+
+    })
+    itemContainer.innerHTML = itemList
 
 
+const trendProducts = [
+        {
+            image:"https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/coffee-asorted.jpg",
+            category:'Groceries',
+            name:'Assorted Coffee',
+            rating:'5star',
+            price:'$35'
+        },
+        {
+            image:'https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/orage-juice-kariz-600x600.jpg',
+            category:'Juice',
+            name:'Fresh Orange Juice',
+            rating:'4star',
+            price:'$18'
+        },
+        {
+            image:'https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/sanitizer-600x600.jpg',
+            category:'Groceries',
+            name:'Hand Sanitizer',
+            rating:'3star',
+            price:'$55'},
+        {
+            image:'https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/red-chillies-600x600.jpg',
+            category:'Groceries',
+            name:'Handpicked Red Chillies',
+            rating:'4star',
+            price:'$65'},
+        
+    ];
+
+    const trendingProduct = document.getElementById('trendingProduct');
+
+    let prodList = ''
+
+    trendProducts.map((item)=> {
+       prodList = prodList + `<div class="product">
+                                <img src="${item.image}"></img>
+                                <p>${item.category}</p>
+                                <h5>${item.name}</h5>
+                                <p>${item.rating}</p>
+                                <p>${item.price}</p>
+                                </div>`
+
+    })
+    trendingProduct.innerHTML = prodList
 
 
 
