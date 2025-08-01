e682d0a0aecdadc6d4644da2a2e73024b1dfd936
Rohini Sureshbabu <rohinimrd@gmail.com>
2025-08-01 12:43:27 +0530

    minor changes

--- contact.js
+++ contact.js
@@ -26,3 +26,61 @@ data.map((item)=>{
                             </div>`
 })
 details.innerHTML = essentials;
+const change=()=>{
+    document.getElementById('hide').style.display === 'none' ? 'inline' : 'none';
+}
+
+const comment = [
+    {
+        comm:'Pulvinar nostrud class cum facilis?',
+        description:'I am item content. Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar leo.'
+},
+{
+    comm : 'Pon excepturi numquam, facilis?',
+    description:'I am item content. Click edit button to change this text. Lorem ipsum dolor sit amet, adipiscing elit. Ut elit tellus, luctus nec mattis, pulvinar dapibus leo.'
+
+},
+ {
+        comm:'Pon excepturi numquam, facilis?',
+        description:'I am item content. Click edit button to change this text. Lorem ipsum dolor sit amet, adipiscing elit. Ut elit tellus, luctus nec mattis, pulvinar dapibus leo.'
+},
+{
+    comm : 'Consequat nesciunt fusce facilisi?',
+    description:'I am item content. Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut luctus nec ullamcorper mattis, pulvinar dapibus leo.'
+
+},
+ {
+        comm:'Pon excepturi numquam, facilis?',
+        description:'I am item content. Click edit button to change this text. Lorem ipsum dolor sit amet, adipiscing elit. Ut elit tellus, luctus nec mattis, pulvinar dapibus leo.'
+},
+{
+    comm : 'Consequat nesciunt fusce facilisi?',
+    description:'I am item content. Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut luctus nec ullamcorper mattis, pulvinar dapibus leo.'
+
+}
+
+]
+
+let question = document.getElementById('review-first');
+let questionComment="";
+comment.map((item)=>{
+    questionComment = questionComment + `<div class='doubts'>
+                                    <div class='question'>
+                                        <h3>${item.comm}</h3>
+                                    </div>
+                                    <span class="showcase">${item.description}</span>
+                                    </div>`
+})
+question.innerHTML = questionComment;
+
+// const show = () => {
+//     document.querySelector('.showcase').style.display = 'block';
+// }
+document.querySelectorAll('.question').forEach((q) => {
+  q.addEventListener('click', () => {
+    const showcase = q.nextElementSibling;
+    showcase.style.display = (showcase.style.display === 'block') ? 'none' : 'block';
+    // showcase.classList.toggle('open');
+
+  });
+});
